b2e00183462d2b54ef33dbc4475485f34f46275b
Muhammad Rafi <88550428+RaxonRafi@users.noreply.github.com>
2023-05-15 13:15:40 +0600

    database config

---
+++ index.js
@@ -0,0 +1,8 @@
+const app = require('./app');
+const db = require('./src/database/db');
+
+app.listen(process.env.PORT,function(){
+
+    console.log("App Running at port: "+process.env.PORT);
+
+})
